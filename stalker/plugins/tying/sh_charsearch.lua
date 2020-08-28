e3d2ce0b1986cc82a4c052d49ed72b4727be8762
Nate Stanley <nathan.stanley.ga@gmail.com>
2020-08-28 17:00:40 -0400

    char search works now, still needs styling

--- stalker/plugins/tying/sh_charsearch.lua
+++ stalker/plugins/tying/sh_charsearch.lua
@@ -1,5 +1,6 @@
 local PLUGIN = PLUGIN
 
+
 if (SERVER) then
 	function PLUGIN:ns1SetupInventorySearch(client, target)
 		local inventory = target:getChar():getInv(client, target)
@@ -122,7 +123,6 @@ if (SERVER) then
 		PLUGIN:stopSearching(client)
 	end)
 else
-	PLUGIN.searchPanels = PLUGIN.searchPanels or {}
 
 	function PLUGIN:CanPlayerViewInventory()
 		if (IsValid(LocalPlayer():getNetVar("searcher"))) then
@@ -130,45 +130,59 @@ else
 		end
 	end
 
+	--STARTING ACTUAL VISUAL STUFF OPENING OK
 	if (nut.version) then
 		netstream.Hook("searchPly", function(target, id)
+			self = vgui.Create( "EditablePanel" )
+			self:SetSize(ScrW(), ScrH())
+			self.noAnchor = CurTime() + .4
+			self.anchorMode = true
+			self:SetPopupStayAtBack(true)
+			self:MakePopup()
+	
 			local targetInv = nut.inventory.instances[id]
 			if (not targetInv) then
-				return netstream.Start("searchExit")
+				self:Remove()
 			end
 
-			local myInvPanel, targetInvPanel
-			local exitLock = true
-			local function onRemove(panel)
-				local other = panel == myInvPanel and targetInvPanel
-					or myInvPanel
-				if (IsValid(other) and exitLock) then
-					exitLock = false
-					other:Remove()
+			self.targetInvPanel = targetInv:show()
+
+			self.targetInvPanel:SetPos(invPosX-(self.targetInvPanel:GetWide()), ScrH()*0.5-(self.targetInvPanel:GetTall()*0.5))
+			self.targetInvPanel.Paint = function() end
+			self.targetInvPanel:SetPopupStayAtBack(true)
+	
+			
+			self.info = vgui.Create("nutCharInfo", self)
+			self.info:setup(self)
+			self.info.quickInventoryPanel:RequestFocus()
+	
+			function self:OnKeyCodePressed(key)
+				--print("f1 pressed cool cool")
+				-- F1 Close
+				local keypress = KEY_F1
+				if(input.LookupBinding("gm_showhelp") != "no value") then
+					keypress = input.GetKeyCode(input.LookupBinding("gm_showhelp"))
 				end
-
-				netstream.Start("searchExit")
-				panel:searchOnRemove()
+		
+				if (key == keypress) then
+					print("ok i close now")
+					netstream.Start("searchExit")
+					self.info:Remove()
+					self.targetInvPanel:Remove()
+					self:Remove()
+				end
 			end
 
-			myInvPanel = LocalPlayer():getChar():getInv():show()
-			myInvPanel:ShowCloseButton(true)
-			myInvPanel.searchOnRemove = myInvPanel.OnRemove
-			myInvPanel.OnRemove = onRemove
 
-			targetInvPanel = targetInv:show()
-			targetInvPanel:ShowCloseButton(true)
-			targetInvPanel:SetTitle(target:Name())
-			targetInvPanel.searchOnRemove = targetInvPanel.OnRemove
-			targetInvPanel.OnRemove = onRemove
 
-			myInvPanel.x = myInvPanel.x + (myInvPanel:GetWide() * 0.5) + 2
-			targetInvPanel:MoveLeftOf(myInvPanel, 4)
 
-			PLUGIN.searchPanels[#PLUGIN.searchPanels + 1] = myInvPanel
-			PLUGIN.searchPanels[#PLUGIN.searchPanels + 1] = targetInvPanel
+			--targetInvPanel:ShowCloseButton(true)
+			--targetInvPanel:SetTitle(target:Name())
+
+			self.targetInvPanel:SetPos(invPosX-(self.targetInvPanel:GetWide()), ScrH()*0.5-(self.targetInvPanel:GetTall()*0.5))
 		end)
 	else
+
 		netstream.Hook("searchPly", function(target, index)
 			local inventory = nut.item.inventories[index]
 
@@ -176,47 +190,53 @@ else
 				return netstream.Start("searchExit")
 			end
 
-			nut.gui.inv1 = vgui.Create("nutInventory")
-			nut.gui.inv1:ShowCloseButton(true)
-			nut.gui.inv1:setInventory(LocalPlayer():getChar():getInv())
+			--start info panel
+			self = vgui.Create( "EditablePanel" )
+			self:SetSize(ScrW(), ScrH())
+			self.noAnchor = CurTime() + .4
+			self.anchorMode = true
+			self:SetPopupStayAtBack(true)
+			self:MakePopup()
+	
+			self.searchInv = vgui.Create("nutInventory")
 
-			PLUGIN.searchPanels[#PLUGIN.searchPanels + 1] = nut.gui.inv1
-
-			local panel = vgui.Create("nutInventory")
-			panel:ShowCloseButton(true)
-			panel:SetTitle(target:Name())
-			panel:setInventory(inventory)
-			panel:MoveLeftOf(nut.gui.inv1, 4)
-			panel.OnClose = function(this)
-				if (IsValid(nut.gui.inv1) and !IsValid(nut.gui.menu)) then
-					nut.gui.inv1:Remove()
+			self.searchInv:setInventory(inventory)
+			self.searchInv:SetPos(invPosX-(self.searchInv:GetWide()), ScrH()*0.5-(self.searchInv:GetTall()*0.5))
+			self.searchInv.Paint = function() end
+			self.searchInv:SetPopupStayAtBack(true)
+	
+			self.info = vgui.Create("nutCharInfo", self)
+			self.info:setup(self)
+			self.info.quickInventoryPanel:RequestFocus()
+			--end info panel
+			
+			function self:OnKeyCodePressed(key)
+				--print("f1 pressed cool cool")
+				-- F1 Close
+				local keypress = KEY_F1
+				if(input.LookupBinding("gm_showhelp") != "no value") then
+					keypress = input.GetKeyCode(input.LookupBinding("gm_showhelp"))
 				end
-
-				netstream.Start("searchExit")
-			end
-
-			local oldClose = nut.gui.inv1.OnClose
-			nut.gui.inv1.OnClose = function()
-				if (IsValid(panel) and !IsValid(nut.gui.menu)) then
-					panel:Remove()
+		
+				if (key == keypress) then
+					print("ok i close now")
+					PLUGIN:stopSearching(client)
+					self.info:Remove()
+					self.targetInvPanel:Remove()
+					self:Remove()
+					
 				end
-
-				netstream.Start("searchExit")
-				nut.gui.inv1.OnClose = oldClose
 			end
-
-			nut.gui["inv"..index] = panel	
-			PLUGIN.searchPanels[#PLUGIN.searchPanels + 1] = panel
+	
+			--panel:ShowCloseButton(true)
+			--panel:SetTitle(target:Name())
+			-- HOW TO CLOSE DUH: netstream.Start("searchExit")
 		end)
 	end
 
+
 	netstream.Hook("searchExit", function()
-		for _, panel in pairs(PLUGIN.searchPanels) do
-			if (IsValid(panel)) then
-				panel:Remove()
-			end
-		end
-		PLUGIN.searchPanels = {}
+
 	end)
 end
 
