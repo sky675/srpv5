6daec65d765e1532e844e8f4d4b68f8ee34251d8
sky <>
2020-08-30 23:29:20 -0400

    f1 grenade

---
+++ stalker/schema/items/weapons/sh_wep_g_frag_f1.lua
@@ -0,0 +1,11 @@
+ITEM.name = "F1 Grenade" --tier 4 grenade
+ITEM.desc = "A fragmentation grenade. Has a medium-sized blast radius and a 3 second timer."
+ITEM.price = 5289
+ITEM.model = "models/eft_grenades/grenade f-1.mdl"
+ITEM.width = 1
+ITEM.height = 1
+ITEM.weight = 0.2
+ITEM.flag = "3"
+ITEM.category = "Weapons - Grenades"
+ITEM.class = "sky_g_frag_f1"
+ITEM.weaponCategory = "grenade"
