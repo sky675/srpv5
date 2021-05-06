19ef96ef6a83c62861b97ed0c7010c9b9ac48f90
sky <>
2021-05-06 17:09:22 -0400

    possible server toggle fix

--- stalker/plugins/servertoggle.lua
+++ stalker/plugins/servertoggle.lua
@@ -7,7 +7,7 @@ if(game.SinglePlayer()) then return end --dont want this setting up in sp
 local pass = "lmaooo" --pass to get in when closed
 local closedStr = " | Open on weekends" --string to add when closed
 
-local curStatus = true --false = open
+PLUGIN.curStatus = PLUGIN.curStatus or true --false = open
 
 nut.command.add("servertoggle", {
 	adminOnly = true,
@@ -15,41 +15,47 @@ nut.command.add("servertoggle", {
 	onRun = function(client, arguments)
 		PLUGIN:ToggleServer()
 
-		return "server has been toggled "..(curStatus and "closed" or "open")
+		return "server has been toggled "..(PLUGIN.curStatus and "closed" or "open")
  	end
 })
 
 if(SERVER) then
 
-	local originalName = originalName or string.Split(GetHostName(), closedStr)[1]
+	PLUGIN.originalName = PLUGIN.originalName or string.Split(GetHostName(), closedStr)[1]
 
 	//if i understand this right, onloaded will run first, need to wait until data is loaded
 	function PLUGIN:PostLoadData()//OnLoaded()
-		if(curStatus) then
+		if(self.curStatus) then
 			RunConsoleCommand("sv_password", pass)
-			RunConsoleCommand("hostname", originalName..closedStr)
+			if(GetHostName():find(closedStr)) then return end --uh ye
+			RunConsoleCommand("hostname", PLUGIN.originalName..closedStr)
 		else
+			--trying to fix
+			if(GetHostName():find(closedStr)) then 
+				RunConsoleCommand("sv_password", "")
+				RunConsoleCommand("hostname", PLUGIN.originalName)
+			end
 			//RunConsoleCommand("sv_password", "")
 			//RunConsoleCommand("hostname", originalName)
 		end
 	end
 
 	function PLUGIN:ToggleServer()
-		curStatus = !curStatus
-		if(curStatus) then
+		self.curStatus = !self.curStatus
+		if(self.curStatus) then
 			RunConsoleCommand("sv_password", pass)
-			RunConsoleCommand("hostname", originalName..closedStr)
+			RunConsoleCommand("hostname", PLUGIN.originalName..closedStr)
 		else
 			RunConsoleCommand("sv_password", "")
-			RunConsoleCommand("hostname", originalName)
+			RunConsoleCommand("hostname", PLUGIN.originalName)
 		end
 	end
 	
 	function PLUGIN:SaveData()
-		self:setData(curStatus, true, true)
+		self:setData(self.curStatus, true, true)
 	end
 
 	function PLUGIN:LoadData()
-		curStatus = self:getData(true, true, true)
+		self.curStatus = self:getData(true, true, true)
 	end
 end
