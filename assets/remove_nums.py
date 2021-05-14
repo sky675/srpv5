df6d684aad22a32aaca0cd0754e124d87659a5cc
Nathan <nathan.stanley.ga@gmail.com>
2021-05-14 16:42:25 -0400

    storage ui

---
+++ assets/remove_nums.py
@@ -0,0 +1,15 @@
+import os
+
+def removeNums(n):
+	start = 0
+	stop = 11
+	nNew = n[0: start:] + n[stop + 1::]
+	return nNew
+
+
+workingDir = 'Q:\\git_srpv5\\srpv5\\assets\\storages'
+for f in os.listdir(workingDir):
+	old_path = os.path.join(workingDir, f)
+	new_path = os.path.join(workingDir, removeNums(f))
+
+	os.rename(old_path, new_path)
