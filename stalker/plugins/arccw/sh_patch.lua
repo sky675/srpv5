0bb0fbd475ed365e7095511c561b2354b6c7631d
sky <>
2021-05-23 02:08:56 -0400

    stupid patching change

--- stalker/plugins/arccw/sh_patch.lua
+++ stalker/plugins/arccw/sh_patch.lua
@@ -1023,3 +1023,11 @@ hook.Add("ArcCW_OnAttLoad", "arcatts_patch", function(v)
 	end
 	--end
 end)
+
+--PLEASE MAKE ArcCW_LoadAtts() A GLOBAL FUNCTION
+local thestupidfunc = hook.GetTable()["PostCleanupMap"]["ArcCW_ReloadAttsDebug"]
+if(thestupidfunc) then
+	GetConVar("arccw_reloadatts_mapcleanup"):SetBool(true)
+	thestupidfunc()
+	GetConVar("arccw_reloadatts_mapcleanup"):SetBool(false)
+end
