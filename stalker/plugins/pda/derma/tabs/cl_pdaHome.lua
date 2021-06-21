34c5e533757a2fd63ec8d63a25388ef024f10569
Nathan Stanley <greekinfl@gmail.com>
2021-06-21 05:39:36 -0400

    center some text boo

--- stalker/plugins/pda/derma/tabs/cl_pdaHome.lua
+++ stalker/plugins/pda/derma/tabs/cl_pdaHome.lua
@@ -1,12 +1,19 @@
 local PANEL = {}
 
 PANEL.tabLabel = "Home"
+local contW, contH = ScrW()*0.289, ScrH()*0.498
+
 
 function PANEL:Init()
     
-    self.sampleTextHome = self:Add("DLabel")
-    self.sampleTextHome:SetText("Home Page - WIP")
-    self.sampleTextHome:SizeToContentsX()
+    self.home = self:Add("DPanel")
+    self.home:SetSize(contW, contH)
+    self.home:SetPaintBackground(false)
+
+    self.wip = self:Add("DLabel")
+    self.wip:SetText("Home Page - Work in Progress")
+    self.wip:SizeToContentsX()
+    self.wip:Center()
 
 end
 
