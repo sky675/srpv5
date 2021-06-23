3279e46e37706a2a519d175112488e2dccbee91d
Nathan <nathan.stanley.ga@gmail.com>
2021-06-23 01:32:14 -0400

    updated help page & rules page to reflect forum

--- stalker/plugins/stalkerui/derma/cl_rules.lua
+++ stalker/plugins/stalkerui/derma/cl_rules.lua
@@ -4,41 +4,106 @@ STALKER_RULES = {
             title = "Introduction",
             body = 
 [[<h1>Introduction:</h1>
-<p>Body</p>]]
+<h3>This rulebook aims to clear up any issues about how RP should be done. It will act as a consultant for questionable interactions that players may have with one another and will also be a tool for them to use if the server has no administration on it to examine the circumstances. Everyone must abide by the rulebook to ensure they has an equal chance within the community. These rules are set in place to make things fair (and for the most part, easy) for all players involved.</h3>
+]]
+
         },
     [2] =
         {
             title = "General",
             body = 
 [[<h1>General Rules:</h1>
-<p>Body</p>]]
+<h3>General rules is a term for Out of Character conduct and how you should treat other players and interact with them. As a player, you cannot:</h3>
+
+<ul>
+<li>Disrespect any member of the playerbase. If you've a problem with someone or wish to debate an action, take it to Steam. This includes name-calling, derogatory language, and generally being a dickhead. The golden rule goes a long way; arguments involving Schrodinger's Douchebag fall under these grounds.</li>
+
+<li>Argue with administration. When a verdict has been given, it's been given. Trying to refute any reasons or points given for the verdict is prohibited, but you can debate the outcome in Steam chat (or in-game PM), kindly. People become more receptive and willing to listen if you're sensible enough to see their reasoning. The judge probably isn't going to feel empathy when you call him an asshole.</li>
+
+<li>Minging/Text Spam. We have a script that displays text on taking damage to make firefights more cohesive for both sides instead of instantaneous OOC ragdoll death. Abusing this system by randomly dumping an entire mag into a man and running away with a giggle is not only annoying, but also a poor waste of ammunition. This also includes binds.</li>
+
+<li>Erotic Roleplay. It's very difficult to filter out age groups within Garry's Mod. This, along with many other reasons, is why we do not allow ERP on the server. The use of a "fade to black" is fine, but not further descriptions should be added.</li>
+
+<li>Metagame. Metagaming is using OOC information to utilize IC. For example, if Monomeme said where he was in OOC and someone from Loner Town beelines right to that location without any actual IC knowledge of where he is. Do not use Discord to metagame. We shouldn't have to say this, but it's happened before, and regardless of who you are, it's not recommended that you do this.</li>
+</ul>
+]]
         },
     [3] =
         {
             title = "Looting",
             body = 
 [[<h1>Looting:</h1>
-<p>Body</p>]]
+<h3>Occasionally, a member on the administration team will fill their inventories on a designated character to drop loot around the map for rookies and or other Stalkers. While finding something is all good and well, there are actually rules to looting instead of running full sprint about the map to get the sick Counter-Strike: Source Water Bottle. When looting, you cannot:</h3>
+<ul>
+<li>Simply pick the item up into your inventory and walk off. To pick up loot, whether it be bread or a weapon, you must provide a /me of you picking it up and either putting it in your bag, holstering it, etc. This is not pointless- the administration team must know how you managed to grab that item. You don't have to provide a paragraph of your dry S.T.A.L.K.E.R hands picking up a luscious water bottle; something small will suffice, but be sure to include the name of the item and the location of it in your /me. If you do not /me picking up items, you will be forced to put them back.</li>
+<li>Pick up unneeded items. We're not going to dictate what is and isn't important to your character- as everyone sees things in a different manner. However, if you're an experienced STALKER and already have a CS-2, CS-1, or even a Sunrise suit and are equipped with both a primary and secondary of moderate caliber- don't be a greedy meme. Leave the pistol or the Mac-10 for someone else to pick up. Loot is for everyone. And if you run around picking everything up, you're taking away from other players. "My character doesn't care about other people and wants money ( ͡° ͜ʖ ͡°)," is not a valid excuse. We understand this might be a vague rule, but we need to ensure that there is, at the very least, an opportunity for all STALKERS to progress.</li>
+<li>Though this is not necessarily a rule, it is a warning: you are ALLOWED to run into NPC areas, but the chance of death might be considerable. If you manage to perish in these encounters, we will not refund ammunition as well as anything you might have expended in that amount of time. Additionally, you take equipment degradation on death. It is heavily frowned upon for [Rasputin, Russia's Favorite Love Machine] to leap into a hive of Bloodsuckers with but a Makarov PM and the sweat upon his brow, but ultimately, that is his choice. Godspeed, you magnificent bastard.</li>
+</ul>
+]]
         },
     [4] =
         {
             title = "Rules of Engagement",
             body = 
 [[<h1>Rules of Engagement:</h1>
-<p>Body</p>]]
+<h3>The Zone is a perilous wasteland filled with all sorts of mad people (either incredibly upset or literally insane). Chances are, you're going to be in a firefight very, very soon. There are rules to go along with this, including a script to prevent actual death to a players (in PvP scenarios). When in a firefight you cannot:</h3>
+<ul>
+<li>
+Ignore being shot. When you are in a firefight with another player, you are not bound by the rule of Shoot to Miss. You're not actually going to kill the person in-game (the script prevents that). The "script" notifies you when you have been shot and where. It also tells you whether or not your armor (if any) protects you from the bullet. It also works vice versa: the script tells you when you've shot someone, and if their body-armor protects them. It looks like this, respectively:
+
+You were hit by a 5.45x39mm round in the abdomen from 41 meters away! You are not protected from the bullet.
+
+You hit someone with a 5.45x39mm round in the abdomen from 41 meters away! They do not appear to be protected from the bullet.
+
+If the script has informed you that you are not protected from the bullet, then you must /me being shot in whichever location has been listed for you. Keep in mind, that a bullet in the leg makes it quite difficult to run, as a bullet in the arm, makes it very difficult to aim correctly. Additionally, if the person you shoot is typing, you will receive a notification of this as well. In an instance where you have already shot a player and they are /me'ing the damage done, any shot that connects after the initial barrage during this /me will likely be ignored. Again, like most things, this is situational and dictated between the group of players involved unless it requires staff attention.
+</li>
+<li>
+Completely disregard being shot with body-armor on. Everyone has seen this at one time or another. All bullets hurt. If your armor protects you, that's great- you have a considerably lesser chance of dying. However, the force of the bullet does not disappear and your stomach isn't going to appreciate what likely amounts to a very robust punch to the gut. Addendum: Pistol rounds on soft body armor are likely to cause severe bruising, even cracked ribs. If wearing rifle plates, the same applies- yet if calibers such as battle rifle calibers are involved (7.62x51, 7.62x54r, 30-06, 7.92 mauser... etc), you are more than likely to be knocked on your ass.
+</li>
+<li>Meta-game. If you are currently in a fight with one individual and someone else appropriately timescales to sneak behind you and shoot you in the back, you cannot simply turn around to "react" to them shooting at you. In example of what NOT to do:
+
+    **Steve swings an uppercut punch towards Greg's jaw, hoping that it would connect.
+    **Greg narrowly dodged the punch, flinching to the side as he took a single step back.
+    **Monomeme quietly snuck up behind Gaburga, his footsteps silent as both Steve and Greg engage in battle.
+    **Steve immediately turns around and shoots the Monomeme.
+    
+    Steve cannot do that. Since the Monomeme has appropriately used a timescale to reach your location and had waited for the round of actions to sneak up upon Steve, Steve wouldn't have heard him at all, nor seen him. There is no 'sixth sense'. Immediately turning around to face someone just because they /me'd is not only meta, but FailRP and is in violation of DA R U L Z.
+</li>
+<li>Complain about being permanently killed. PKs are issued when:
+
+- You attack another player and end up dead because of such in a planned encounter (premeditated MURDER).
+-If you attack another player unprovoked (no gun aiming, shooting, etc.) and they kill you.
+-If you are Temporarily Killed three times (3 Strikes rule).
+-In-Character drama that results in one or more parties death.
+</li>
+<li>Temporary kills are issued when:
+
+- You die in any sort of event or are shot to death by a faction outside of their territory.
+- Die by an NPC or another player that allows you to take a Temporary Kill.
+- Attack faction members like Duty, Monolith, Freedom, and the UKM in their respective bases/territories and die because of such.
+
+Temporary kills result in the deceased having to RP a serious injury after the TK and are expected to act as if they had this debilitating injury. You can choose to go out on a mission while injured; however, if you die as a result of this injury, you will more than likely be issued a PK.
+</li>
+<li>Assume that because you died by an NPC, you think you're completely fine. If you're in an event or looting and come across a den or maybe single NPC and they shoot you to death, that means you have to RP a serious physical injury. If you get shot by them and don't die, then we merely expect that you act as if your armor absorbed the bullet AFTER the gunfight is over. Don't just stand there and /me while the NPCs shoot you to death; they are NPCs and deserve no player rites of engagement. If you would like to RP something different after being shot by an NPC and not being killed, you are free to do so- it is your character and you have agency.</li>
+<li>
+Double-action. That means something along the lines of "/me runs up the hill towards the Mono-meme, taking aim and firing his weapon." You cannot do that. You must give the other person a chance to react. The correct way to RP the previous statement would be:
+
+    **Kyle runs up the hill to find a better advantage on the distracted Mono-meme.
+    **Mono-meme continues to fire at someone completely different, screaming something.
+    **Kyle takes his stance upon the top of the hill, now behind the Mono-meme; shooting at him from this position.
+</li>    
+</ul>
+]]
         },
     [5] =
-        {
-            title = "Deaths",
-            body = 
-[[<h1>Deaths:</h1>
-<p>Body</p>]]
-        },
-    [6] =
         {
             title = "Anomalies",
             body = 
 [[<h1>Anomalies:</h1>
-<p>Body</p>]]
+<h3>Anomalies may have artifacts in them from time to time that can be utilized or sold for rubles. All deaths resulting from a player trying to grab an artifact from an anomaly and dying because of it result in Temporary Kills. It was an active choice that you made to dive in there or do whatever it was you did and it killed you. Anomalies are pretty circumstantial, but we are going to enforce this rule because, well.. If you run into a burner anomaly on accident, it's the Zone. These things happen.
+</h3>
+<p>Some anomalies may require you to /roll depending on how you try to reach the artifact within. Generally, we don't utilize /roll, and you will likely never have to do this unless administration asks you to do it. If they do, understand that the anomaly in which you are interacting with is volatile and dangerous. High risk, high reward scenarios are a key part in STALKER. Even the most legendary of STALKERS know the pain of INSTANTANEOUS LIGHTNING DEATH.
+    </p>
+]]
         },
 }
