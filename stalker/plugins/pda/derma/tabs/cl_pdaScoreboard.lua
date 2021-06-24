d326bed4371f1f93012308120cd1a90d3cde6b47
Nathan <nathan.stanley.ga@gmail.com>
2021-06-24 03:13:42 -0400

    Styled online list in pda

--- stalker/plugins/pda/derma/tabs/cl_pdaScoreboard.lua
+++ stalker/plugins/pda/derma/tabs/cl_pdaScoreboard.lua
@@ -2,6 +2,8 @@ local PANEL = {}
 
 PANEL.tabLabel = "Online List"
 
+local contW, contH = ScrW()*0.289, ScrH()*0.498
+
 function PANEL:UpdateScoreboard(tbl)
 	if(self.pdaLabel) then
 		self.pdaLabel:Remove()
@@ -13,49 +15,40 @@ function PANEL:UpdateScoreboard(tbl)
 	local y = 10
 	for k,v in ipairs(tbl) do
 
-        self.pan[k] = vgui.Create("DPanel", self)
-		self.pan[k]:SetSize(self:GetWide(), ScrH()*0.04)
-		self.pan[k]:DockMargin(2,(first == false and 22 or 2),2,2)
-		first = true
-		self.pan[k]:Dock(TOP)
+        self.pan[k] = self.scoreboard:Add("DPanel")
+        self.pan[k]:Dock(TOP)
+        self.pan[k]:DockMargin(0,3,0,3)
+        self.pan[k]:DockPadding(10,0,10,0)
+        self.pan[k]:SetTall(32)
 
-
-		--handle 0.25 wide, status 0.6 wide, avail 0.15 wide
-
-        self.pan[k].handle = vgui.Create("DLabel", self.pan[k])      
-        self.pan[k].handle:SetPos(10, y)
-        self.pan[k].handle:SetSize(self.pan[k]:GetWide()*0.28, 0)
+        self.pan[k].handle = self.pan[k]:Add("DLabel")
+        self.pan[k].handle:SetText(tbl[k].handle) 
         self.pan[k].handle:SetFont("nutSmallFont")
-        self.pan[k].handle:SetText(tbl[k].handle)
-        self.pan[k].handle:DockMargin(6,2,4,2)
-        self.pan[k].handle:SetContentAlignment(4)
-        first = true
-        self.pan[k].handle:Dock(LEFT)
+        self.pan[k].handle:SetWide(contW/3)
+        self.pan[k].handle:SetContentAlignment(5)
         self.pan[k].handle:SetWrap(true)
+        self.pan[k].handle:Dock(LEFT)
 
-
-        self.pan[k].title = vgui.Create("DLabel", self.pan[k])        
-        self.pan[k].title:SetPos(10, y)
-        self.pan[k].title:SetSize(self.pan[k]:GetWide()*0.6, 0)
+        self.pan[k].title = self.pan[k]:Add("DLabel")
+        self.pan[k].title:SetText(tbl[k].title) 
         self.pan[k].title:SetFont("nutSmallFont")
-        self.pan[k].title:SetText(tbl[k].title)
-        self.pan[k].title:DockMargin(2,2,2,2)
-        self.pan[k].title:SetContentAlignment(4)
-        first = true
-        self.pan[k].title:Dock(FILL)
+        self.pan[k].title:SetWide(contW/3)
+        self.pan[k].title:SetContentAlignment(5)
         self.pan[k].title:SetWrap(true)
-		
+        self.pan[k].title:Dock(LEFT)
 
-        self.pan[k].avail = vgui.Create("DLabel", self.pan[k])         
-        self.pan[k].avail:SetPos(10, y)
-        self.pan[k].avail:SetSize(self.pan[k]:GetWide()*0.12, 0)
+        self.pan[k].avail = self.pan[k]:Add("DLabel")
+        self.pan[k].avail:SetText(tbl[k].avail) 
+        if(string.lower(tbl[k].avail) == "active") then
+            self.pan[k].avail:SetTextColor(Color(140,204,110))
+        elseif (string.lower(tbl[k].avail) == "idle") then
+            self.pan[k].avail:SetTextColor(Color(204,202,110))
+        end
         self.pan[k].avail:SetFont("nutSmallFont")
-        self.pan[k].avail:SetText(tbl[k].avail)
-        self.pan[k].avail:DockMargin(2,2,6,2)
-        self.pan[k].avail:SetContentAlignment(6)
-        first = true
-        self.pan[k].avail:Dock(RIGHT)
+        self.pan[k].avail:SetWide(contW/3)
+        self.pan[k].avail:SetContentAlignment(5)
         self.pan[k].avail:SetWrap(true)
+        self.pan[k].avail:Dock(LEFT)
 
 	end
 end
@@ -64,8 +57,11 @@ end
 function PANEL:Init()
     	
 	local y = 10
+    self.scoreboard = self:Add("DScrollPanel")
+    self.scoreboard:SetSize(contW, contH)
+    self.scoreboard:SetPaintBackground(false)
 
-    self.pdaLabel = self:Add("DLabel")
+    self.pdaLabel = self.scoreboard:Add("DLabel")
 	self.pdaLabel:SetPos(10, y)
 	self.pdaLabel:SetFont("nutSmallFont")
 	self.pdaLabel:SetText("Loading List")
@@ -73,8 +69,6 @@ function PANEL:Init()
 	self.pdaLabel:Dock(FILL)
 
     self.pdaLabel:SetWrap(true)
-
-    self:AddItem(self.pdaLabel)
 
 
 
