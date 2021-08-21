8bab0fdbcaa2f2660dd36dfc739e928c2a8abc6f
sky <w>
2021-08-21 10:44:32 -0400

    fixed hostname being past name limit

--- addons/skybase/lua/autorun/servertoggler.lua
+++ addons/skybase/lua/autorun/servertoggler.lua
@@ -3,8 +3,8 @@ if(game.SinglePlayer()) then return end
 serverToggle = serverToggle or {}
 
 local pass = "shazbot" --pass to get in when closed
-local closedStr = " | https://discord.gg/DFKbGSu6FH" --string to add when closed
-local defName = "Kolobok | STALKER Serious Roleplay"
+local closedStr = "| https://discord.gg/DFKbGSu6FH" --string to add when closed
+local defName = "Kolobok|STALKER Serious Roleplay"
 
 serverToggle.curStatus = serverToggle.curStatus or (cookie.GetNumber("serverTog", 1) == 1)  --false = open
 
