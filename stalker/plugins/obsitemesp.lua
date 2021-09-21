264928a9b955c48a716dda3111df6583c4102aeb
sky <w>
2021-09-20 22:27:12 -0400

    added money to item esp

--- stalker/plugins/obsitemesp.lua
+++ stalker/plugins/obsitemesp.lua
@@ -46,6 +46,20 @@ if CLIENT then
 	
 					nut.util.drawText("Radiation Spot", x, y-size, ColorAlpha(Color(0,220,0,255),alpha), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, nil, alpha)	
 				end
+				if(v:GetClass() == "nut_money") then
+					local scrPos = v:GetPos():ToScreen()
+					local marginx, marginy = sy*.1, sy*.1
+					local x, y = math.Clamp(scrPos.x, marginx, sx - marginx), math.Clamp(scrPos.y, marginy, sy - marginy)
+					local distance = client:GetPos():Distance(v:GetPos())
+					local factor = 1 - math.Clamp(distance/1024, 0, 1)
+					local size = math.max(10, 32*factor)
+					local alpha = math.Clamp(255*factor, 80, 255)
+	
+					surface.SetDrawColor(30, 30, 30, alpha)
+					surface.DrawRect(x - size/2, y - size/2, size, size)
+	
+					nut.util.drawText("Money: "..nut.currency.get(v:getAmount()), x, y-size, ColorAlpha(Color(220,220,220,255),alpha), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, nil, alpha)	
+				end
 				--]]
             end
         end
